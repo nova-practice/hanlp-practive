d66f9bcd2489cc770b7c402aefbebd769f9857a1
quantumcs <quantumcs@sina.com>
2018-04-26 23:27:16 +0800

    nlp-baiduapi

---
+++ nlp-baiduapi/src/main/java/org/quartz/nlp/api/baidu/NlpBaiduapiApplication.java
@@ -0,0 +1,12 @@
+package org.quartz.nlp.api.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpBaiduapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpBaiduapiApplication.class, args);
+	}
+}
---
+++ nlp-baiduapi/src/test/java/org/quartz/nlp/api/baidu/NlpBaiduapiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.quartz.nlp.api.baidu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NlpBaiduapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
